52bea2406bb4c2a405b27b74e1ce34fc347b6d5c
SemSh <shnaider.sem@gmail.com>
2022-07-10 15:54:57 +0300

    Update main.tf

--- assets/main.tf
+++ assets/main.tf
@@ -68,3 +68,30 @@ resource "aws_iam_user_policy_attachment" "attachment" {
 output "s3_bucket_arn" {
   value = aws_s3_bucket.bucket.arn
 }
+
+resource "aws_iam_policy" "policy" {
+  name        = "${random_pet.pet_name.id}-policy"
+  description = "My test policy"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListAllMyBuckets"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.bucket.arn}"
+    }
+  ]
+}
+EOT
+}
