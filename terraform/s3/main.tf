f7a8dd8726c35d8d035830381b880708d09004be
SemSh <shnaider.sem@gmail.com>
2022-07-10 16:25:50 +0300

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -19,7 +19,7 @@ data "aws_iam_user" "input_user" {
 
 resource "aws_s3_bucket" "bucket" {
   bucket = var.name
-  acl    = "public-read"
+  aws_s3_bucket_acl    = "public-read"
   force_destroy = true
 
   tags = {
