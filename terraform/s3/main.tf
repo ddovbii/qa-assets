7802123c96fa0076c3dc36771e9947174692d81f
SemSh <shnaider.sem@gmail.com>
2022-07-10 16:22:09 +0300

    Update main.tf

--- terraform/s3/main.tf
+++ terraform/s3/main.tf
@@ -30,34 +30,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 # CREATE USER and POLICY
-resource "aws_iam_policy" "policy" {
-  count = "${var.user == "none" ? 0 : 1}"
-  name        = "s3_access_${var.name}"
-  path        = "/"
-  description = "Policy to access S3 Module"
-  # one more comment here
 
-  # Terraform expression result to valid JSON syntax.
-  policy = jsonencode({
-    Version: "2012-10-17",
-    Statement: [
-        {
-        Effect: "Allow",
-        Action: ["s3:ListBucket"],
-        Resource: ["arn:aws:s3:::${var.name}"]
-        },
-        {
-        Effect: "Allow",
-        Action: [
-            "s3:PutObject",
-            "s3:GetObject",
-            "s3:DeleteObject"
-        ],
-        Resource: ["arn:aws:s3:::${var.name}/*"]
-        }
-    ]
-  })
-}
 
 resource "aws_iam_user_policy_attachment" "attachment" {  
     count = "${var.user == "none" ? 0 : 1}"
