7c78f46c379a69b94a10b4b5ce7bd069e2fe9fc6
asafyahalom <49859118+asafyahalom@users.noreply.github.com>
2022-08-15 16:08:12 +0300

    Create variables.tf

---
+++ s3/variables.tf
@@ -0,0 +1,33 @@
+# comment
+variable "name" {
+    description = "Name of S3 bucket"
+    type = string
+    default = "asaf-bucket2"
+}
+
+variable "region" {
+    description = "Region where to create resources" 
+    type = string
+    default = "eu-west-1"
+}
+
+variable "acl" {
+    description = "Canned ACL to apply to the bucket. Default is private."
+    type = string
+    default = "public-read"
+
+    validation {
+        condition = contains(["private", "public-read", "public-read-write", "aws-exec-read", "authenticated-read", "log-delivery-write"], var.acl)
+        error_message = "Provided ACL is not a recognized canned type. The page https://docs.aws.amazon.com/AmazonS3/latest/userguide/acl-overview.html#canned-acl has available options."
+    }
+}
+
+variable "user" {
+    description = "Username to assign permissions for S3 bucket to. If left blank, will not create permissions."
+    default = "none"
+}
+
+variable "test" {
+    description = "Double slash test"
+    default = "wubba labba "
+}
