88e626842f9080442172191db0f45093ddb11749
asafyahalom <49859118+asafyahalom@users.noreply.github.com>
2022-08-18 15:49:05 +0300

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -23,7 +23,7 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = true
 
   tags = {
-    Name        = "I changed"
+    Name        = "I changed again"
     Environment = "Dev"
     AMAZING_TAG = "AMAZING_VALUE"
   }
